cfb7a614f6a158394686e08d893006d2f4ec2289
josevillacres <josepajaro99@gmail.com>
2021-12-01 18:52:01 -0500

    Primer COMMIT 18:50

---
+++ app/src/main/java/deitel/com/jurado_villacres/MainActivity.java
@@ -0,0 +1,14 @@
+package deitel.com.jurado_villacres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
