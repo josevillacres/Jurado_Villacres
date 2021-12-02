3ebf7ff873b796e3f06475e758744899638c0316
josevillacres <josepajaro99@gmail.com>
2021-12-01 19:51:25 -0500

    commit "19:50"

--- app/src/main/java/deitel/com/jurado_villacres/MainActivity.java
+++ app/src/main/java/deitel/com/jurado_villacres/MainActivity.java
@@ -2,7 +2,9 @@ package deitel.com.jurado_villacres;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -23,8 +25,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnSiguiente=findViewById(R.id.buttonSiguiente);
+        btnMostrarResultado=findViewById(R.id.buttonMostrarResultados);
+        editTextNombre = findViewById(R.id.editTextNombre);
+        editTextApellido = findViewById(R.id.editTextApellido);
+        editTextExponente = findViewById(R.id.editTextExponente);
+        editTextBase = findViewById(R.id.editTextBase);
+        editTextPotencia = findViewById(R.id.editTextPotencia);
+        editTextFactorial = findViewById(R.id.editTextFactorial);
+        // bloqueo
+        editTextFactorial.setEnabled(false);
+        editTextPotencia.setEnabled(false);
+        editTextApellido.setEnabled(false);
+        editTextNombre.setEnabled(false);
+        editTextBase.setEnabled(false);
+        editTextExponente.setEnabled(false);
+
     }
 
+    public void siguente(View view){
+        Intent intent = new Intent(this,SegundoActivity.class);
+        startActivity(intent);
+    }
+
+    public void mostrarResultados(View view){
+
+    }
 
 
 }
