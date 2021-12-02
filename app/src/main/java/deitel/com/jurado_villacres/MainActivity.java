170ff87a80359dda20ceb93ca3b497a965c06386
josevillacres <josepajaro99@gmail.com>
2021-12-01 19:35:25 -0500

    commit "19:35"

--- app/src/main/java/deitel/com/jurado_villacres/MainActivity.java
+++ app/src/main/java/deitel/com/jurado_villacres/MainActivity.java
@@ -3,12 +3,28 @@ package deitel.com.jurado_villacres;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextNombre;
+    private EditText editTextApellido;
+    private EditText editTextBase;
+    private EditText editTextExponente;
+    private EditText editTextPotencia;
+    private EditText editTextFactorial;
+    private Button btnSiguiente;
+    private Button btnMostrarResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSiguiente=findViewById()
     }
+
+
+
 }
